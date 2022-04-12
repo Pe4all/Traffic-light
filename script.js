7402df1643eea62ef21c33c6a01edcbf63854722
Pe4all <shefov.max@yandex.ru>
2022-04-13 02:36:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const button = document.getElementById("clickButton");
+
+let i = 1;
+
+button.addEventListener('click', function () {
+    // Между function и () не должно быть пробела
+
+    if (i === 1) {
+        let elem2 = document.getElementById('secondCircle');
+
+        elem2.style.background = 'yellow';
+        elem2.style.boxShadow = '0 0 10px yellow';
+        // Когда на момент написания кода известны стили, гораздо правильнее и проще устанавливать стили элементам через классы, а не через свойство style
+
+        let elem1 = document.getElementById('firstCircle');
+        elem1.style.background = 'grey';
+        elem1.style.boxShadow= '0 0 10px gray';
+        i++;
+    } else if (i === 2) {
+        let elem2 = document.getElementById('secondCircle');
+        elem2.style.background = 'grey';
+        elem2.style.boxShadow = '0 0 10px grey';
+
+        let elem3 = document.getElementById('thirdCircle');
+        elem3.style.background = 'green';
+        elem3.style.boxShadow= '0 0 10px green';
+        i++;
+    } else if (i === 3) {
+        let elem2 = document.getElementById('secondCircle');
+        elem2.style.background = 'yellow';
+        elem2.style.boxShadow = '0 0 10px yellow';
+
+        let elem3 = document.getElementById('thirdCircle');
+        elem3.style.background = 'grey'
+        elem3.style.boxShadow= '0 0 10px gray';
+        i++;
+    } else if (i === 4) {
+        let elem1 = document.getElementById('firstCircle');
+        elem1.style.background = 'red';
+        elem1.style.boxShadow= '0 0 10px red';
+
+        let elem2 = document.getElementById('secondCircle');
+        elem2.style.background = 'grey';
+        elem2.style.boxShadow = '0 0 10px gray';
+        i = 1;
+    }
+
+console.log(i);
+});
